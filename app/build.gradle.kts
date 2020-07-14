abeac90ce62fb737d9038ca3d8d1d3f9e0ab7c4e
KurozeroPB <root@kurozeropb.info>
2020-07-14 12:09:31 +0200

    Bump dependecies & version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,19 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+}
+
 object Versions {
     private const val versionMajor = 1
-    private  const val versionMinor = 0
+    private  const val versionMinor = 1
     private const val versionPatch = 0
     private val versionClassifier = null
 
-    const val minSdkVersion = 24
-    const val targetSdkVersion = 29
+    const val minSdk = 24
+    const val targetSdk = 29
 
-    fun generateVersionCode(): Int = minSdkVersion * 10000000 + versionMajor * 10000 + versionMinor * 100 + versionPatch
+    fun generateVersionCode(): Int = minSdk * 10000000 + versionMajor * 10000 + versionMinor * 100 + versionPatch
 
     fun generateVersionName(): String {
         var versionName = "${versionMajor}.${versionMinor}.${versionPatch}"
@@ -20,47 +26,66 @@ object Versions {
     }
 }
 
-plugins {
-    id("com.android.application")
-    kotlin("android")
-    kotlin("android.extensions")
-}
-
 android {
-    compileSdkVersion(Versions.targetSdkVersion)
+    compileSdkVersion(Versions.targetSdk)
+
     defaultConfig {
         applicationId = "xyz.kurozero.nekosmoe"
-        minSdkVersion(Versions.minSdkVersion)
-        targetSdkVersion(Versions.targetSdkVersion)
+        minSdkVersion(Versions.minSdk)
+        targetSdkVersion(Versions.targetSdk)
         versionCode = Versions.generateVersionCode()
         versionName = Versions.generateVersionName()
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
+    @Suppress("COMPATIBILITY_WARNING")
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions.apply {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
 }
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50")
+    implementation(kotlin("stdlib"))
+
+    implementation("androidx.core:core-ktx:1.3.0")
     implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.core:core-ktx:1.1.0")
-    implementation("com.google.android.material:material:1.0.0")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    implementation("com.github.bumptech.glide:glide:4.9.0")
-    implementation("org.jetbrains.anko:anko:0.10.8")
-    implementation("com.github.kittinunf.fuel:fuel:2.2.0")
-    implementation("com.google.code.gson:gson:2.8.5")
-    implementation("com.facebook.fresco:fresco:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
+
+    implementation("com.github.kittinunf.fuel:fuel:2.2.3")
+    implementation("com.github.kittinunf.fuel:fuel-android:2.2.3")
+    implementation("com.github.kittinunf.fuel:fuel-coroutines:2.2.3")
+    implementation("com.github.kittinunf.fuel:fuel-gson:2.2.3")
     implementation("com.github.stfalcon:frescoimageviewer:0.5.0")
+    implementation("com.github.bumptech.glide:glide:4.10.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.10.0")
+
+    implementation("com.google.android.material:material:1.1.0")
+    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.facebook.fresco:fresco:1.9.0")
     implementation("com.squareup.picasso:picasso:2.71828")
     implementation("com.hendraanggrian:pikasso:0.2")
+
     implementation("de.hdodenhof:circleimageview:3.0.1")
-    testImplementation("junit:junit:4.12")
+
+    implementation("org.jetbrains.anko:anko:0.10.8")
+    implementation("org.jetbrains.anko:anko-design:0.10.8")
+
+    testImplementation("junit:junit:4.13")
     androidTestImplementation("androidx.test.ext:junit:1.1.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,15 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
         google ()
         jcenter()
+        maven(url = "http://dl.bintray.com/kotlin/kotlin-eap")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.6.0-beta01")
-        classpath(kotlin("gradle-plugin", version = "1.3.50"))
+        classpath("com.android.tools.build:gradle:4.2.0-alpha04")
+        classpath(kotlin("gradle-plugin", version = "1.4-M2"))
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -17,6 +19,15 @@ allprojects {
     repositories {
         google()
         jcenter()
+        maven(url = "https://jitpack.io")
+        maven(url = "http://dl.bintray.com/kotlin/kotlin-eap")
+    }
+
+    // -Xopt-in=kotlin.RequiresOptIn
+    tasks.withType(KotlinCompile::class).all {
+        kotlinOptions {
+            freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
+        }
     }
 }
 
