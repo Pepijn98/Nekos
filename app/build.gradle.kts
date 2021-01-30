509096056d53a4c27b79e51dfd62c523642e23fd
Pepijn98 <root@kurozeropb.info>
2021-01-30 22:00:25 +0100

    v1.1.1
    - Update dependencies
    - Add kotlinx coroutines libraries (fixes crashes on certain devices)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 object Versions {
     private const val versionMajor = 1
     private  const val versionMinor = 1
-    private const val versionPatch = 0
+    private const val versionPatch = 1
     private val versionClassifier = null
 
     const val minSdk = 24
-    const val targetSdk = 29
+    const val targetSdk = 30
 
     fun generateVersionCode(): Int = minSdk * 10000000 + versionMajor * 10000 + versionMinor * 100 + versionPatch
 
@@ -60,21 +60,19 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(kotlin("stdlib"))
 
-    implementation("androidx.core:core-ktx:1.3.0")
-    implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
+    implementation("androidx.core:core-ktx:1.3.2")
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
 
-    implementation("com.github.kittinunf.fuel:fuel:2.2.3")
-    implementation("com.github.kittinunf.fuel:fuel-android:2.2.3")
-    implementation("com.github.kittinunf.fuel:fuel-coroutines:2.2.3")
-    implementation("com.github.kittinunf.fuel:fuel-gson:2.2.3")
+    implementation("com.github.kittinunf.fuel:fuel:2.3.1")
+    implementation("com.github.kittinunf.fuel:fuel-android:2.3.1")
+    implementation("com.github.kittinunf.fuel:fuel-coroutines:2.3.1")
+    implementation("com.github.kittinunf.fuel:fuel-gson:2.3.1")
     implementation("com.github.stfalcon:stfalcon-imageviewer:1.0.1")
-    implementation("com.github.bumptech.glide:glide:4.10.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.10.0")
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.11.0")
 
-    implementation("com.google.android.material:material:1.1.0")
+    implementation("com.google.android.material:material:1.2.1")
     implementation("com.google.code.gson:gson:2.8.6")
     implementation("com.facebook.fresco:fresco:1.9.0")
     implementation("com.squareup.picasso:picasso:2.71828")
@@ -84,8 +82,10 @@ dependencies {
 
     implementation("org.jetbrains.anko:anko:0.10.8")
     implementation("org.jetbrains.anko:anko-design:0.10.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.2")
 
-    testImplementation("junit:junit:4.13")
-    androidTestImplementation("androidx.test.ext:junit:1.1.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+    testImplementation("junit:junit:4.13.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         maven(url = "http://dl.bintray.com/kotlin/kotlin-eap")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.0-alpha04")
+        classpath("com.android.tools.build:gradle:7.0.0-alpha05")
         classpath(kotlin("gradle-plugin", version = "1.4-M2"))
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
