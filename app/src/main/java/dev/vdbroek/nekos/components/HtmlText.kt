43866b3790b011218e3ad32836142e4346b34e37
Pepijn van den Broek <pepijn@vdbroek.dev>
2022-05-30 17:55:58 +0200

    Set HtmlText text color

--- app/src/main/java/dev/vdbroek/nekos/components/HtmlText.kt
+++ app/src/main/java/dev/vdbroek/nekos/components/HtmlText.kt
@@ -4,8 +4,11 @@ import android.text.method.LinkMovementMethod
 import android.widget.TextView
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
+import dev.vdbroek.nekos.ui.theme.NekoColors
+import dev.vdbroek.nekos.ui.theme.ThemeState
 
 @Composable
 fun HtmlText(html: String, modifier: Modifier = Modifier) {
@@ -15,6 +18,8 @@ fun HtmlText(html: String, modifier: Modifier = Modifier) {
             TextView(context)
         },
         update = {
+            it.setTextColor(if (ThemeState.isDark) NekoColors.md_theme_dark_onBackground.toArgb() else NekoColors.md_theme_light_onBackground.toArgb())
+            it.setLinkTextColor(if (ThemeState.isDark) NekoColors.md_theme_dark_primary.toArgb() else NekoColors.md_theme_light_primary.toArgb())
             it.text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT)
             it.movementMethod = LinkMovementMethod.getInstance()
         }
